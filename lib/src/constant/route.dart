2725978f6179e2ba77ef90d228d643067712cc15
Elvis <igieborelvis@gmail.com>
2024-07-31 09:03:31 +0100

    unnecessary updates

--- lib/src/constant/route.dart
+++ lib/src/constant/route.dart
@@ -7,13 +7,11 @@ import 'package:uog/src/students/dashboard/notification/notifications.dart';
 import 'package:uog/src/students/dashboard/qrcode/qrcode.dart';
 
 import '../features/auth/presentation/views/signIn.dart';
-import '../features/dashboard/dashboard.dart';
 import '../features/onboarding/presentation/views/onboarding.dart';
 
 // import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-import '../students/dashboard/dashboard.dart';
 import '../students/dashboard/student-dashboard.dart';
 
 class Routes {
@@ -25,7 +23,6 @@ class Routes {
   static const staffDashboard = 'staffDashboard';
   static const studentsScreen = 'studentsScreen';
 
-
   //routes
   static final routes = <String, Widget Function(BuildContext)>{
     // splash : (context) =>  const SplashScreen(),
@@ -34,19 +31,16 @@ class Routes {
     onboarding: (context) => const OnBoarding(),
     studentsScreen: (context) => const StudentsScreen(),
     staffDashboard: (context) => const StaffDashBoard(),
-    signIn: (context) =>
-        StreamBuilder<User?>(
-            stream: FirebaseAuth.instance.authStateChanges(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                final User? user = snapshot.data;
-                return const StudentDashBoard();
-              } else {
-                return const SignIn();
-              }
-            }
-        ),
+    signIn: (context) => StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final User? user = snapshot.data;
+            return const StudentDashBoard();
+          } else {
+            return const SignIn();
+          }
+        }),
     dashboard: (context) => const StudentDashBoard(),
   };
-
 }
